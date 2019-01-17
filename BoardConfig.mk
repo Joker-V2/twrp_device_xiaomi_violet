ae514779660781e55d0ff44a7076c8b239abb08a
mauronofrio <poronofrio@gmail.com>
2019-01-17 20:00:38 +0100

    violet: Define notch dimentions
    * Add a Black Bar on the top of the screen to
    hide the notch and make time visible
    
    Change-Id: I6862af2ab2de3a440b8d41524c4b5c617a0ef10b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,6 +121,8 @@ TARGET_USES_LOGD := true
 TARGET_USES_MKE2FS := true
 TW_INCLUDE_RESETPROP := true
 TW_EXCLUDE_TWRPAPP := true
+TW_Y_OFFSET := 80
+TW_H_OFFSET := -80
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 
