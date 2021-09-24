7451e53c2654a973d368b51425419ee0af9b2fcb
emufan4568 <geoster3d@gmail.com>
2021-09-24 19:06:43 +0300

    violet: Enable TW_INCLUDE_RESETPROP
    * This flag enables the compilation of the resetprop binary which
    can be used in the twrp-common repo instead of setprop. This fixes
    the following errors:
    
    fscrypt_initialize_systemwide_keys returned fail

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,6 +118,7 @@ TW_DEFAULT_BRIGHTNESS := 1200
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 TARGET_USES_MKE2FS := true
+TW_INCLUDE_RESETPROP := true
 TW_EXCLUDE_TWRPAPP := true
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
