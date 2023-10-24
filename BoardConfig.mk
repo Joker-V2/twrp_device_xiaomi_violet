ee13b2e8010da3e6780d879082a346afd5d593d5
Nipin NA <nipinna0@gmail.com>
2023-10-24 21:59:27 +0530

    violet: Align status bar items properly

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,7 +145,9 @@ TARGET_USES_LOGD := true
 TARGET_USES_MKE2FS := true
 TW_INCLUDE_RESETPROP := true
 TW_EXCLUDE_TWRPAPP := true
-TW_Y_OFFSET := 80
-TW_H_OFFSET := -80
+TW_STATUS_ICONS_ALIGN := center
+TW_CUSTOM_CPU_POS := "50"
+TW_CUSTOM_CLOCK_POS := "340" 
+TW_CUSTOM_BATTERY_POS := "800"
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
